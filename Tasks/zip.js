eae3f85ff890bb51c26595fab679e50d9d1fd263
MariaSalamatova <salamatovamary@gmail.com>
2025-01-13 22:06:50 +0200

    Replacing
    Array.from provides a more concise way to iterate over a range of indices.

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -2,11 +2,8 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
 const zip = function (a = [], b = []) {
-  const result = [];
-  for (let i = 0; i < Math.min(a.length, b.length); i++) {
-    result.push([a[i], b[i]]);
-  }
-  return result;
+  const length = Math.min(a.length, b.length); 
+  return Array.from({ length }, (_, i) => [a[i], b[i]]);
 };
 
 module.exports = zip;
