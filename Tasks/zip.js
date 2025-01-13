dc3324bba77af340f0128d0a0a30c715139a7795
MariaSalamatova <salamatovamary@gmail.com>
2025-01-13 22:09:00 +0200

    Refactor
    Converted the function to an arrow function to make it more concise.

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,7 +1,7 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-const zip = function (a = [], b = []) {
+const zip = (a = [], b = []) => {
   const length = Math.min(a.length, b.length); 
   return Array.from({ length }, (_, i) => [a[i], b[i]]);
 };
