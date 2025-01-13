090524c99149784ffa613eb8e56bf7a9bdc55da2
MariaSalamatova <salamatovamary@gmail.com>
2025-01-13 20:25:33 +0200

    Logic
    Combined redundant logic into a concise and efficient form. The if condition for max === undefined is replaced with a straightforward check if min === max.

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,12 +2,10 @@
 // Generate random integer value in given range
 
 random = (min, max = min) => {
-  if (max === undefined) {
-    max = min;
+  if (min === max) {
     return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
   }
+    return min + Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 module.exports = random;
