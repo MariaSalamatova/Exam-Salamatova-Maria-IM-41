e3948a21c96a327f6692af8009b4e1df0588f269
MariaSalamatova <salamatovamary@gmail.com>
2025-01-13 21:30:00 +0200

    Add return
    Added an early return when separator is falsy.

--- Tasks/section.js
+++ Tasks/section.js
@@ -2,8 +2,9 @@
 // Split string into array by the first occurrence of separator
 
 SectionString = (str, separator) => {
+  if (!separator) return [str, ''];
  const index = str.indexOf(separator);
-  if (index < 0 || separator == '') {
+  if (index < 0) {
     return [str, ''];
   } else {
     return [str.slice(0, index), str.slice(index + separator.length)];
